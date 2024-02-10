2dd895a47423010df09f6d3df452b9c9c8db4cda
Robotics <team935programming@gmail.com>
2024-02-09 18:23:27 -0600

    Added shooter subsystem

---
+++ src/main/cpp/subsystems/ShooterSubsystem.cpp
@@ -0,0 +1,41 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+#include "subsystems/ShooterSubsystem.h"
+#include "Constants.h"
+
+
+ShooterSubsystem::ShooterSubsystem() : m_shooterMotor{IntakeConstants::kMotorID, IntakeConstants::kMotorType} {
+  // Implementation of subsystem constructor goes here.
+}
+
+frc2::CommandPtr ShooterSubsystem::ExampleMethodCommand() {
+  // Inline construction of command goes here.
+  // Subsystem::RunOnce implicitly requires `this` subsystem.
+  return RunOnce([/* this */] { /* one-time action goes here */ });
+}
+
+bool ShooterSubsystem::ExampleCondition() {
+  // Query some boolean state, such as a digital sensor.
+  return false;
+}
+
+void ShooterSubsystem::Periodic() {
+  // Implementation of subsystem periodic method goes here.
+}
+
+void ShooterSubsystem::TurnOn() {
+  // Turn on the motor. 
+  m_shooterMotor.Set(1.0);
+}
+
+void ShooterSubsystem::TurnOff() {
+  // Turn off the motor.
+  m_shooterMotor.Set(0.0);
+}
+
+void ShooterSubsystem::SetMotorPower(double power) {
+  // Sets the motor's power (between -1.0 and 1.0). 
+  m_shooterMotor.Set(power);
+}
---
+++ src/main/include/subsystems/ShooterSubsystem.h
@@ -0,0 +1,48 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+#pragma once
+
+#include <frc2/command/CommandPtr.h>
+#include <frc2/command/SubsystemBase.h>
+#include <rev/CANSparkMax.h>
+
+class ShooterSubsystem : public frc2::SubsystemBase {
+ public:
+  ShooterSubsystem();
+
+  /**
+   * Example command factory method.
+   */
+  frc2::CommandPtr ExampleMethodCommand();
+
+  /**
+   * An example method querying a boolean state of the subsystem (for example, a
+   * digital sensor).
+   *
+   * @return value of some boolean subsystem state, such as a digital sensor.
+   */
+  bool ExampleCondition();
+
+  /**
+   * Will be called periodically whenever the CommandScheduler runs.
+   */
+  void Periodic() override;
+
+  // Turn on shooter
+  void TurnOn();
+
+  //Turn off shooter
+  void TurnOff();
+
+  //Sets the motor's power (between -1.0 and 1.0).
+  void SetMotorPower(double power);
+
+ private:
+  // Components (e.g. motor controllers and sensors) should generally be
+  // declared private and exposed only through public methods.
+
+  // Motor Controllers
+  rev::CANSparkMax m_shooterMotor;
+};
