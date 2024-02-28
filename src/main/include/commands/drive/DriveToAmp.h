49a2495c81c097e34c8afec5a384cdd632f55fc9
Erik Kaufman <kaufmane558@gmail.com>
2024-02-27 21:46:22 -0600

    started declaring command

--- src/main/include/commands/drive/DriveToAmp.h
+++ src/main/include/commands/drive/DriveToAmp.h
@@ -6,3 +6,13 @@
 #include <frc2/command/CommandHelper.h>
 
 #include "subsystems/DriveSubsystem.h"
+
+
+class DriveToAmp : public frc2::CommandHelper<frc2::Command, DriveToAmp> {
+public:
+
+
+
+private:
+
+};
